9005cff1301bf1c3c9fa477f3b3d8fc71a4da96b
kirill123 <kirillgor12506@gmail.com>
2023-02-03 17:11:15 +0300

    Door

--- Assets/Scripts/AI/DoorOpenClose.cs
+++ Assets/Scripts/AI/DoorOpenClose.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class DoorOpenClose : MonoBehaviour
 {
-    [SerializeField] private GameObject _oneState;
-    [SerializeField] private GameObject _twoState;
+    [SerializeField] private Animation _anim;
 
-    private void Start()
+    [SerializeField] private AnimationClip _openAnimClip;
+    [SerializeField] private AnimationClip _closeAnimClip;
+
+    private bool isOpen = true;
+
+    public void OpenClose(bool isOpen)
     {
-        OpenClose();
+        _anim.Play("" + (isOpen ? _openAnimClip.name : _closeAnimClip.name));
     }
 
-    public void OpenClose(bool isOpen = false)
+    public void OpenClose()
     {
-        _oneState.SetActive(!isOpen);
-        _twoState.SetActive(isOpen);
+        _anim.Play("" + (isOpen ? _openAnimClip.name : _closeAnimClip.name));
+        isOpen = !isOpen;
     }
 }
--- Assets/Scripts/Player/DoorOpenPlayer.cs
+++ Assets/Scripts/Player/DoorOpenPlayer.cs
@@ -8,17 +8,13 @@ public class DoorOpenPlayer : MonoBehaviour
 
     [SerializeField] private Transform _cameraPoint;
 
-    private bool SetOpen = true;
-
     private Vector3 direction => Camera.main.transform.TransformDirection(Vector3.forward);
 
     private void Update()
     {
-
-        if (Physics.Raycast(Camera.main.transform.position, direction, out RaycastHit hit, _distancy, _layreObject) && hit.collider.TryGetComponent(out DoorOpenClose _ddor) && Input.GetKeyDown(KeyCode.E))
+        if (Physics.Raycast(Camera.main.transform.position, direction, out RaycastHit hit, _distancy, _layreObject) && hit.collider.TryGetComponent(out DoorOpenClose _door) && Input.GetKeyDown(KeyCode.E))
         {
-            _ddor.OpenClose(SetOpen);
-            SetOpen = !SetOpen;
+            _door.OpenClose();
         }
     }
 }
