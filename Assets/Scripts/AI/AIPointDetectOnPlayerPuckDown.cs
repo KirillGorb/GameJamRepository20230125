eb8fc38bdb654fba92d73d7ea2d769cf95683c2f
NexysStormcloud <Nexys.Stormcloud@gmail.com>
2023-02-04 14:07:38 +0300

    Update AIPointDetectOnPlayerPuckDown.cs

--- Assets/Scripts/AI/AIPointDetectOnPlayerPuckDown.cs
+++ Assets/Scripts/AI/AIPointDetectOnPlayerPuckDown.cs
@@ -15,7 +15,7 @@ public class AIPointDetectOnPlayerPuckDown : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, point) <= _distancyDetectNewPoint && Physics.Raycast(transform.position, point, out RaycastHit hit) && hit.collider == null)
         {
-            _moveAI.AddDestinationInterrupt(DestinationPoint.AddNew(point));
+            _moveAI.AddDestinationInterrupt(DestinationPoint.AddNew(point, animMode:1, stopDist:1, name:"Suspicious", duration:7));
         }
     }
 }
